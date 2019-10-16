a84c7e82da6a678f06259c54bd275b9462ad4cf7
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2019-10-16 14:56:09 +0700

    Antd customize theme

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '91EE16',
+            '@link-color': '91EE16'
+        }
+    })
+)

5ea0b9ef6fb38397d3dfb624310a3f752dbd2e05
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2019-10-19 09:46:16 +0700

    Pun, coins to header

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { Layout, Menu, Avatar } from 'antd';
+import { Layout, Menu, Avatar, Popover, Icon, Divider } from 'antd';
 import MessengerPopover from 'components/MessengerPopover';
 import NotificationPopover from 'components/NotificationPopover';
+import Heart from 'elements/Icon/Heart';
+import Coin from 'elements/Icon/Coin';
 import Routes from 'config/router.config';
 import styles from './index.module.less';
 
@@ -10,7 +12,7 @@ const { Header } = Layout;
 const MenuItem = Menu.Item;
 
 const DejavuHeader = (props) => {
-    const { location } = props;
+    const { location, history } = props;
     let selectedKey = location.pathname.slice(1);
     return (
         <Header className={styles.header}>
@@ -32,12 +34,44 @@ const DejavuHeader = (props) => {
                 }
             </Menu>
             <div className={styles.rightContent}>
+                <Popover
+                    arrowPointAtCenter={false}
+                    popupAlign={{ offset: [0, 11] }}
+                    popupClassName={styles.content}
+                    visible
+                    content={(
+                        <div>
+                            <div className={styles.profile} onClick={() => history.push('/account')}>
+                                <Icon type="user" style={{ color: 'yellowgreen' }} /><span style={{ marginLeft: 5, fontSize: 12 }}>Profile</span>
+                            </div>
+                            <div className={styles.pun}>
+                                <Heart size={"14"} /><span style={{ marginLeft: 5, fontSize: 12 }}>12577</span>
+                            </div>
+                            <div className={styles.coins}>
+                                <Coin size={"14"} /><span style={{ marginLeft: 5, fontSize: 12 }}>4069</span>
+                            </div>
+                            <Divider style={{ width: '100%', margin: 5 }}/>
+                            <div className={styles.logout}>
+                                <Icon type="logout" style={{ color: 'yellowgreen' }}/><span style={{ marginLeft: 5, fontSize: 12 }}>Log out</span>
+                            </div>
+                        </div>
+                    )}
+                >
+                    <div className={styles.account}>
+                        <Avatar style={{ backgroundColor: '#87d068' }} icon="user" size={28} />
+                        <span className={styles.name}>Yuna Choi</span>
+                    </div>
+                </Popover>
                 <div className={styles.messengerPopover}><MessengerPopover /></div>
                 <div className={styles.notificationPopover}><NotificationPopover /></div>
-                <div className={styles.account}>
-                    <Avatar style={{ backgroundColor: '#87d068' }} icon="user" size={28} />
-                    <span className={styles.name}>Yuna Choi</span>
+
+                {/* <div className={styles.pun}>
+                    <Heart size={"28"} /><span className={styles.punAmount}>12577</span>
                 </div>
+                <div className={styles.coins}>
+                    <Coin size={"10"} /><span className={styles.coinAmount}>4069</span>
+                </div> */}
+                
             </div>
         </Header>
     )
---
+++ src/pages/Account/Account.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import styles from './Account.module.less';
+
+class Account extends PureComponent {
+    render() {
+        return (
+            <div>This is account page</div>
+        )
+    }
+}
+
+export default Account;
